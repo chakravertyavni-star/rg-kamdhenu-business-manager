82b01eeec4b2068f655075be9578734e9ac30615
Avni Chakraverty <chakravertyavni@gmail.com>
2026-06-21 20:47:18 +0530

    navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useEffect } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { useLocation } from "react-router-dom";
 import "../styles/Navbar.css";
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
-
+  const location = useLocation();
+    useEffect(() => {
+      setMenuOpen(false);
+    }, [location]);
+    
   return (
     <nav className="navbar">
 
@@ -26,19 +32,47 @@ export default function Navbar() {
 
       <div className={`nav-links ${menuOpen ? "active" : ""}`}>
 
-        <Link to="/dashboard">Dashboard</Link>
-
-        <Link to="/revenue">Revenue</Link>
-
-        <Link to="/expenses">Expenses</Link>
-
-         <Link to="/reports">
-            Reports
+         <Link
+          to="/dashboard"
+          onClick={() => setMenuOpen(false)}
+        >
+          Dashboard
         </Link>
 
-        <Link to="/analytics">Analytics</Link>
+        <Link
+          to="/revenue"
+          onClick={() => setMenuOpen(false)}
+        >
+          Revenue
+        </Link>
 
-        <Link to="/exports">Exports</Link>
+        <Link
+          to="/expenses"
+          onClick={() => setMenuOpen(false)}
+        >
+          Expenses
+        </Link>
+
+        <Link
+          to="/reports"
+          onClick={() => setMenuOpen(false)}
+        >
+          Reports
+        </Link>
+
+        <Link
+          to="/analytics"
+          onClick={() => setMenuOpen(false)}
+        >
+          Analytics
+        </Link>
+
+        <Link
+          to="/exports"
+          onClick={() => setMenuOpen(false)}
+        >
+          Exports
+        </Link>
 
       </div>
 
